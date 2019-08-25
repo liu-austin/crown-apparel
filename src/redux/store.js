65a1769cb6da77194653aee6c88933d708c8acb0
liu-austin <austinliu279@gmail.com>
2019-08-25 02:41:03 -0700

    added conditional logger middleware if environment is development

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,14 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import { persistStore } from 'redux-persist';
 
-const middlewares = [logger];
+const middlewares = [];
+
+// If in development environment use logger middleware,
+// but not in production environment
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
+
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
